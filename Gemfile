5e4039d30ecc6f15b9d45c7a7b79fa2042598fe6
Aimerick Desdoit <aimerick@lunaweb.fr>
2013-11-23 09:42:02 +0100

    premier commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'http://rubygems.org'
+
+gem "icalendar", "~> 1.4.5"
+gem 'activesupport', '~> 4.0'
+gem 'slugify', '~> 1.0.5'
---
+++ reporting.rb
@@ -0,0 +1,62 @@
+require 'rubygems'
+
+env = (ENV["RACK_ENV"] || ENV["RAILS_ENV"] || "development").to_sym
+
+# Set up gems listed in the Gemfile.
+gemfile = File.expand_path('../Gemfile', __FILE__)
+begin
+  ENV['BUNDLE_GEMFILE'] = gemfile
+  require 'bundler'
+  Bundler.setup
+rescue Bundler::GemNotFound => e
+  STDERR.puts e.message
+  STDERR.puts "Try running `bundle install`."
+  exit!
+end if File.exist?(gemfile)
+
+# If you have a Gemfile, require the gems listed there, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(:default, env) if defined?(Bundler)
+
+require 'active_support'
+require 'active_support/core_ext/object/conversions'
+require 'icalendar'
+
+def time_format(seconds)
+  minutes = seconds / 60
+  hours = (minutes / 60)
+  minutes = minutes % 60
+  "#{hours}h#{minutes > 0 ? minutes : ''}"
+end
+
+cal_file = File.open(Dir[File.expand_path('../*.ics', __FILE__)].first)
+cals = Icalendar.parse(cal_file)
+cal = cals.first
+
+start_min, start_max = [Time.now.beginning_of_week.utc, Time.now.end_of_week.utc]
+
+durations = {}
+excluded_sequence = [2]
+cal.events.each do |event|
+  if event.dtstart > start_min && event.dtend < start_max && !excluded_sequence.include?(event.sequence)
+    duration = event.dtend.to_i - event.dtstart.to_i
+    key = event.summary.slugify_trim
+    durations[key] ||= {:title => event.summary, :duration => 0}
+    durations[key][:duration] += duration
+  end
+end
+
+durations = durations.collect { |a, b| [a, b] }.sort { |a, b| a[0] <=> b[0] }
+durations = ActiveSupport::OrderedHash[durations]
+
+content = ActiveSupport::OrderedHash.new
+durations.collect do |dc_title, infos|
+  duration = time_format(infos[:duration])
+  title = infos[:title].downcase.to_s.gsub(/(\A| )./) { |m| m.upcase }
+  content[title] = duration
+end
+
+max_length = content.keys.map { |t| t.length }.max
+content.each do |title, duration|
+  puts "#{title.ljust(max_length, ' ')}   #{duration}"
+end
